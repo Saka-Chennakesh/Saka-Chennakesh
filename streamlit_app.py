81e30c046529ff1215853ca2ae0e913767544e58
Saka Chennakesh <168456615+Saka-Chennakesh@users.noreply.github.com>
2024-04-30 16:19:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,11 +35,11 @@ def run():
         selection = st.session_state[key]
         st.write(f"Selection changed to {selection}")
     
-    selected3 = option_menu(None, ["Summary", "Skills",  "Projects", 'ORG',"EDU"],
+    selected3 = option_menu(None, ["Summary", "Skills",  "Projects", 'ORG',"Education"],
                             icons=['list-task', 'list-task', "list-task", 'gear','list-task'],
                             on_change=on_change, key='menu_5', orientation="horizontal")
     selected3        
-    if selected3=="Short Profile":
+    if selected3=="Summary":
         st.markdown(
             """
     ### Profile Summary
