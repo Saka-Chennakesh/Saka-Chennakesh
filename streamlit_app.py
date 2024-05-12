ee8e041e725c09be0ebc2a54b7c4326ff51895d0
Saka Chennakesh <168456615+Saka-Chennakesh@users.noreply.github.com>
2024-05-12 15:49:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,28 @@ def run():
     )
 
     st.write("# Saka Chennakesh 👋")
+    def add_logo():
+        st.markdown(
+            """
+            <style>
+                [data-testid="stSidebarNav"] {
+                    background-image: url(https://github.com/Saka-Chennakesh/Saka-Chennakesh/blob/main/photo.jpg);
+                    background-repeat: no-repeat;
+                    padding-top: 120px;
+                    background-position: 20px 20px;
+                }
+                [data-testid="stSidebarNav"]::before {
+                    content: "My Company Name";
+                    margin-left: 20px;
+                    margin-top: 20px;
+                    font-size: 30px;
+                    position: relative;
+                    top: 100px;
+                }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
      
 #    st.sidebar.success("Select a demo above.")
     #"Profile", "Skills",  "Projects", 'Org'
