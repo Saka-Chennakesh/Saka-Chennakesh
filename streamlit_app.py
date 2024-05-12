8ebcdd4e9e0f5f1a2d3433a03e1d402601d61751
Saka Chennakesh <168456615+Saka-Chennakesh@users.noreply.github.com>
2024-05-12 14:57:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -154,7 +154,7 @@ def run():
     if selected3=="ORG":
 
         dataset = pd.read_csv("org.csv", encoding='ISO-8859-1')
-        st.markdown(dataset.to_html(escape=False), unsafe_allow_html=True)
+        st.markdown(dataset.to_html(escape=False))
     if selected3=="Education":
 
         dataset = pd.read_csv("edu.csv")
