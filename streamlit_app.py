1e32a97250653626ffe184d421405cf12512d6cb
Saka Chennakesh <168456615+Saka-Chennakesh@users.noreply.github.com>
2024-06-06 23:13:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,7 +171,7 @@ def run():
     if selected3=="ORG":
          with st.expander("Organisational Experience|	Organisation Name|	City|	Designation"):
             
-            dataset = pd.read_csv("org.csv", encoding='ISO-8859-1',index_col = False)[1]
+            dataset = pd.read_csv("org.csv", encoding='ISO-8859-1',index_col = False).filter(items=[1],axis=0)
             #st.markdown(dataset.to_html(escape=False), unsafe_allow_html=True)
             st.markdown(dataset.style.hide(axis="index").to_html(), unsafe_allow_html=True)
             
