bbf9405459dab3c5adfe7f1ce8fa0df377ded419
Saka Chennakesh <168456615+Saka-Chennakesh@users.noreply.github.com>
2024-06-06 23:21:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,10 +171,10 @@ def run():
     if selected3=="ORG":
          # with st.expander("Evoke Technologies"):
             
-            dataset = pd.read_csv("org.csv", encoding='ISO-8859-1',index_col = False).filter(items=[1],axis=0)
+            dataset = pd.read_csv("org.csv", encoding='ISO-8859-1',index_col = False)["Organisational Experience", "Organisation Name", "City","Designation"].filter(items=[1],axis=0)
             #st.markdown(dataset.to_html(escape=False), unsafe_allow_html=True)
-            st.markdown(dataset["Organisational Experience", "Organisation Name", "City","Designation"].style.hide(axis="index").to_html(), unsafe_allow_html=True)
-            st.markdown(dataset["Roles & Responsibility"].style.hide(axis="index").to_html(), unsafe_allow_html=True)
+            st.markdown(dataset.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+            # st.markdown(dataset["Roles & Responsibility"].style.hide(axis="index").to_html(), unsafe_allow_html=True)
             
     if selected3=="Education":
 
