58a28862c88182b1ff6fc03710ff8a781b50b83b
Saka Chennakesh <168456615+Saka-Chennakesh@users.noreply.github.com>
2025-02-13 10:51:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ def run():
     ### Profile Summary
     ##### Resume Headline:
     Data Science, Engineering and Analytics Manager/Architect with 12 years of experience in Machine Learning, Financial Modelling, Statistical Modelling, NLP, Deep Learning, MLOps, Data Engineering and Data Analytics using Python, R, SAS, pyspark, git, docker, AWS, Azure, Tableau, Alteryx, Web Frameworks, SQL, NoSQL, Excel etc.,
-    Currently working at WELLS FARGO, Bangalore as Vice President -Lead Quant Model Solutions Specialist and PhD Scholar in Deep Learning at Capital University, Jharkhand.
+    Currently working at WELLS, Bangalore as Vice President and PhD Scholar in Deep Learning at Capital University, Jharkhand.
 
     ##### Business Domains: 
     Worked/Lead with deep technical, data science, strategic and analytical skills for: [Retail, Manufacturer, Banking (Credit Risk, Market Risk), Insurance, Irrigation, Pharma and Legal] Domain Clients 
